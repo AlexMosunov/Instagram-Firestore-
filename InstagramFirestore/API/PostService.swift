a1299aa678e882acca206b4d131b2f8362da9461
Oleksii Mosunov <alexmosunov@gmail.com>
2021-12-18 23:41:34 +0200

    implemented adding posts functionality with imagePicker

---
+++ InstagramFirestore/API/PostService.swift
@@ -0,0 +1,26 @@
+//
+//  PostService.swift
+//  InstagramFirestore
+//
+//  Created by Alex Mosunov on 18.12.2021.
+//
+
+import UIKit
+import Firebase
+
+struct PostService {
+    
+    static func uploadPost(caption: String, image: UIImage, completion: @escaping FirestoreCompletion) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        ImageUploader.uploadImage(image: image) { imageUrl in
+            let data = ["caption" : caption,
+                        "timestamp" : Timestamp(date: Date()),
+                        "likes" : 0,
+                        "imageUrl" : imageUrl,
+                        "ownerUid" : uid] as [String : Any]
+            COLLECTION_POSTS.addDocument(data: data, completion: completion)
+        }
+    }
+    
+}
